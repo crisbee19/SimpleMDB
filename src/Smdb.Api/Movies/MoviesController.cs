156b8ac9d2fa7aad6a1fca1c41c5c681b89c9672
crisbee19 <cpadilla5633@interbayamon.edu>
2025-12-11 13:45:11 -0400

    Add MoviesController

--- src/Smdb.Api/Movies/MoviesController.cs
+++ src/Smdb.Api/Movies/MoviesController.cs
@@ -24,6 +24,31 @@ public class MoviesController
       await JsonUtils.SendPagedResultResponse(req, res, props, result, page, size);
       await next();
    }
+   // curl -X POST "http://localhost:8080/api/v1/movies" -H "Content-Type:  application/json" -d "{ \"id\": -1, \"title\": \"Inception\", \"year\": 2010, \"description\": \"A skilled thief who enters dreams to steal secrets.\" }" 
+   public async Task CreateMovie(HttpListenerRequest req,
+   HttpListenerResponse res, Hashtable props, Func<Task> next)
+   {
+      var text = (string)props["req.text"]!;
+      var movie = JsonSerializer.Deserialize<Movie>(text,
+      JsonSerializerOptions.Web);
+      var result = await movieService.CreateMovie(movie!);
+      await JsonUtils.SendResultResponse(req, res, props, result);
+      await next();
+   }
+   // curl -X GET "http://localhost:8080/api/v1/movies/1" 
+
+   public async Task ReadMovie(HttpListenerRequest req, HttpListenerResponse res,
+     Hashtable props, Func<Task> next)
+   {
+      var uParams = (NameValueCollection)props["req.params"]!;
+      int id = int.TryParse(uParams["id"]!, out int i) ? i : -1;
+
+      var result = await movieService.ReadMovie(id);
+
+      await JsonUtils.SendResultResponse(req, res, props, result);
+
+      await next();
+   }
    // curl -X PUT "http://localhost:8080/api/v1/movies/1" -H "Content-Type: application/json" -d "{ \"title\": \"Joker 2\", \"year\": 2020, \"description\": \"A man that is a joke.\" }"
    public async Task UpdateMovie(HttpListenerRequest req,
    HttpListenerResponse res, Hashtable props, Func<Task> next)
